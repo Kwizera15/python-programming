4724d07f4ffc6a8ad373f0d9cfc5fb82039f5c9e
Kwizera15 <kwizerad523@gmail.com>
2024-11-07 15:27:58 +0200

    Add files via upload

---
+++ the sum of even and odd1.py
@@ -0,0 +1,15 @@
+def sum_even_odd(numbers):
+    even_sum = 0
+    odd_sum = 0
+    for num in numbers:
+        if num % 2 == 0:
+            even_sum += num  
+        else:
+            odd_sum += num  
+    
+    return (even_sum, odd_sum)
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+result = sum_even_odd(numbers)
+print(result)
