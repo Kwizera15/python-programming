42560ca3a023db8e414fdda67c3f0a2abb7da59d
Kwizera15 <kwizerad523@gmail.com>
2024-12-11 23:39:45 +0200

    Delete sum of odd,even.py

--- sum of odd,even.py
+++
@@ -1,9 +0,0 @@
-def sum_even_odd(lst):
-    even_sum = sum(num for num in lst if num % 2 == 0)
-    odd_sum = sum(num for num in lst if num % 2 != 0)
-    return (even_sum, odd_sum)
-
-
-my_list = [1, 2, 3, 4, 5, 6]
-result = sum_even_odd(my_list)
-print(result)
